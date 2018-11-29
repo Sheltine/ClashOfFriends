9d25788c5e81dbd9a268fd3b48d976fff57f8f70
Schnoudli <leo.cortes@heig-vd.ch>
2018-11-29 16:01:46 +0100

    User and Challenge basic model

---
+++ Challenge.js
@@ -0,0 +1,9 @@
+class Challenge{
+  constructor(user1, user2, category, startDate, endDate){
+    this.user1 = user1;
+    this.user2 = user2;
+    this.category = category;
+    this.startDate = startDate;
+    this.endDate = endDate;
+  }
+}
---
+++ User.js
@@ -0,0 +1,9 @@
+class User{
+  constructor(name, surname, username, email, password){
+    this.name = name;
+    this.surname = surname;
+    this.username = username;
+    this.email = email;
+    this.password = password;
+  }
+}
