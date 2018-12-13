9f45f2e36121123580b9466844026626c342894e
Schnoudli <leo.cortes@heig-vd.ch>
2018-12-13 18:04:31 +0100

    Including schema into schema #schemaception

--- backend/src/Schemas/ChallengeSchema.js
+++ backend/src/Schemas/ChallengeSchema.js
@@ -1,18 +1,21 @@
 const mongoose = require('mongoose');
+const ChallengeCategorySchema = require('./ChallengeCategorySchema');
+const ChallengeFormatSchema = require('./ChallengeFormatSchema');
+const ChallengeThemeSchema = require('./ChallengeThemeSchema');
 
 const { Schema } = mongoose;
 
 const challengeSchema = new Schema({
   categorie:{
-    type: challengeCategorySchema,
+    type: ChallengeCategorySchema,
     required: true
   },
   format:{
-    type: challengeFormatSchema,
+    type: ChallengeFormatSchema,
     required: true
   },
   theme:{
-    type: challengeThemeSchema,
+    type: ChallengeThemeSchema,
     required: true
   }
 }, { timestamps: { createdAt: 'createdAt', updatedAt: 'updatedAt' } });
