26eb9aafe3e0a945a0eadd2180917871ceeac584
Schnoudli <leo.cortes@heig-vd.ch>
2019-01-17 14:30:36 +0100

    Login page

--- frontend/src/components/Form_connexion.jsx
+++ frontend/src/components/Form_connexion.jsx
@@ -4,6 +4,11 @@ import { FormGroup, FormControl, ControlLabel, Button, Alert } from 'react-boots
 import ApolloClient from 'apollo-boost';
 import gql from 'graphql-tag';
 import { Query, ApolloProvider } from 'react-apollo';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import AppBar from 'material-ui/AppBar';
+import RaisedButton from 'material-ui/RaisedButton';
+import TextField from 'material-ui/TextField';
+import axios from 'axios';
 
 const { BACKEND_URL } = require('../config');
 
@@ -73,6 +78,7 @@ function getErrorMsg(username, password, submitted) {
     return <p />;
 }
 
+/*
 class FormConnexion extends Component {
     constructor(props) {
         super(props);
@@ -144,5 +150,79 @@ class FormConnexion extends Component {
     );
   }
 }
+*/
+
+class FormConnexion extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+        username: '',
+        password: '',
+        submitted: false,
+    };
+
+    this.handleInputChange = this.handleInputChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+}
+
+
+handleInputChange(event) {
+const value = event.target.value;
+const name = event.target.name;
+
+this.setState({
+    [name]: value,
+    submitted: false,
+
+});
+}
+
+handleSubmit(event) {
+    // Debug purpose
+    // eslint-disable-next-line react/destructuring-assignment
+    this.setState({
+     submitted: true,
+    });
+  console.log('submitted: ', this.state.username);
+  event.preventDefault();
+  }
+
+  render() {
+      return (
+        <div>
+          {localStorage.clear()}
+          {sessionStorage.clear()}
+          <MuiThemeProvider>
+            <div>
+            <form onSubmit={this.handleSubmit}>
+              <TextField
+                hintText="Enter your Username"
+                floatingLabelText="Username"
+                name="username"
+                onChange={this.handleInputChange}
+              />
+              <br />
+              <TextField
+                type="password"
+                hintText="Enter your Password"
+                floatingLabelText="Password"
+                onChange={(event, newValue) => this.setState({ password: newValue })}
+                name="password"
+                onChange={this.handleInputChange}
+              />
+              <br />
+              <RaisedButton type="submit" label="Submit" primary style={style} />
+              </form>
+            </div>
+          </MuiThemeProvider>
+          <p>{getErrorMsg(this.state.username, this.state.password, this.state.submitted)}</p>
+        </div>
+      );
+    }
+  }
+
+  const style = {
+   margin: 15,
+  };
 
 export default FormConnexion;
