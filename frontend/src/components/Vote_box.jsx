6a02ef86a5ce9c5644b9ca1a5c8bc303420a088d
Schnoudli <leo.cortes@heig-vd.ch>
2019-01-22 18:21:39 +0100

    Setting vote box state, need DB access

--- frontend/src/components/Vote_box.jsx
+++ frontend/src/components/Vote_box.jsx
@@ -3,21 +3,36 @@ import '../App.css';
 
 
 class VoteBox extends Component {
+  constructor(props, context) {
+    super(props, context);
+    this.state = {
+      challengerName: 'Challenger',
+      nbVotes: 0,
+    };
+    this.addVote = this.addVote.bind(this);
+  }
+
+  // eslint-disable-next-line class-methods-use-this
+  addVote() {
+    // TODO : DB requests and access
+    this.setState({nbVotes: this.state.nbVotes + 1 });
+  }
+
   render() {
     return (
       <div>
         <center>
           <div className="main">
             <p>
-              <button className="add-button" type="button">&#x1f44d;</button>
+              <button className="add-button" type="button" onClick={this.addVote}>&#x1f44d;</button>
             </p>
             <p>
-              <h2>Challenger2</h2>
+              <h2>{this.state.challengerName}</h2>
             </p>
             <p />
             <div>
               <div>
-                <strong>42</strong><br />
+                <strong>{this.state.nbVotes}</strong><br />
                 <span className="details">votes</span>
               </div>
             </div>
