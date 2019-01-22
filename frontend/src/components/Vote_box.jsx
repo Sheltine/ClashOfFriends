ec02934d518ad34c26a688a334275f47dc7e2585
Schnoudli <leo.cortes@heig-vd.ch>
2019-01-22 08:48:24 +0100

    Vote box one user

--- frontend/src/components/Vote_box.jsx
+++ frontend/src/components/Vote_box.jsx
@@ -1,11 +1,27 @@
 import React, { Component } from 'react';
+import '../App.css';
 
 
 class VoteBox extends Component {
   render() {
     return (
       <div>
-          things
+        <div class="main">
+          <p>
+            <button class="add-button" >&#x1f44d;</button>
+          </p>
+          <p>
+            <h2>Challenger1</h2>
+          </p>
+          <p>
+          </p>
+          <div>
+            <div>
+              <strong>2k</strong><br />
+              <span class="details">votes</span>
+            </div>
+          </div>
+        </div> 
       </div>
     );
   }
